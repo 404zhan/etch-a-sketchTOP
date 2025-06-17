a04ca343251973539f185f177d16244bf94a0e67
Alamgir-09 <onedirection123124@gmail.com>
2025-06-18 00:30:22 +0530

    added hovering effect for sketching, and a new grid btn, but still working on its functionality

--- script.js
+++ script.js
@@ -6,9 +6,26 @@ for(let i=0; i<16; i++){
      container.appendChild(rowDiv);
     for(let j=0; j<16; j++){
        const aBox = document.createElement("div");
+       aBox.setAttribute("class", "divboxes");
        aBox.style.cssText = "border: 2px solid black; height: 50px; width: 50px";
        rowDiv.appendChild(aBox);
     }
-
 }
+const allBoxes = document.querySelectorAll(".divboxes");
+allBoxes.forEach(box=>{
+    box.addEventListener("mouseover", function(event){
+    event.target.style.backgroundColor="black";})
+});
+
+const newGridBtn = document.querySelector("#newgrid");
+newGridBtn.addEventListener("click", function(event){
+   let newGrid = window.prompt("Enter the number of squares per side: ");
+   if(newGrid>100){
+    alert("The number of boxes must be less than 10,000!");
+   }
+   else{
+    container.innerHTML="";
+   }
+})
+
 
